2219ba7eb214f350599c091ddcd34c98c3f12d1d
Suraj Singh <absurajsinghchamiyal@gmail,com>
2022-06-12 20:55:09 +0530

    fixing href

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="menuBar">
       <div className="logo">
-        <a href="">
+        <a href="/">
           ATools<span>.</span>
         </a>
       </div>
